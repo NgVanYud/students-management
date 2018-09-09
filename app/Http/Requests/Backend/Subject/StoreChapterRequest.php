3a4f3f11dc5f8716e39b1ad07488db1ffbe69840
duynv <duy.nv.96@gmail.com>
2018-09-09 19:47:21 +0700

    edit request for authentication chapter

---
+++ app/Http/Requests/Backend/Subject/StoreChapterRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Backend\Subject;
+
+use App\Exceptions\GeneralException;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Route;
+
+class StoreChapterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return ($this->user()->isAdmin() || $this->user()->isQuizMaker());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $subject = $this->subject;
+        $name = $this->name;
+        $action = Route::getCurrentRoute()->getActionMethod();
+        if($action == "store") {
+            return [
+                'name' => [
+                    'required',
+                    'min:5',
+                    'max:191',
+                    Rule::unique('chapters')->where(function ($query) use($subject, $name) {
+                        return $query->where('subject_id', $subject->id)
+                            ->where('name', $name);
+                    }),
+                ]
+            ];
+        } else if($action == "update") {
+            return [
+                'name' => 'required|min:5|max:191|unique:chapters,name,'.$this->chapter->id,
+            ];
+        }
+        throw new GeneralException(__('exceptions.backend.subjects.chapters.validate_error'));
+    }
+}
