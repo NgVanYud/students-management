04ba77d3e6738dd76fa76c848bfe14166aa6f9aa
duynv <duy.nv.96@gmail.com>
2018-09-24 20:30:30 +0700

    edit view dashboard backend

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -10,7 +10,54 @@
                     <strong>{{ __('strings.backend.dashboard.welcome') }} {{ $logged_in_user->name }}!</strong>
                 </div><!--card-header-->
                 <div class="card-block">
-                    {!! __('strings.backend.welcome') !!}
+                    {{--{!! __('strings.backend.welcome') !!}--}}
+                    <div class="row">
+                        <div class="col-xl-3 col-sm-6 mb-5">
+                            <div class="card text-white bg-primary o-hidden">
+                                <div class="card-body">
+                                    <div class="card-body-icon">
+                                        <i class="fas fa-users fa-3x"></i>
+                                    </div>
+                                    <div class="mr-5">{{$users_count}} Users!</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-sm-6 mb-5">
+                            <div class="card text-white bg-warning o-hidden">
+                                <div class="card-body">
+                                    <div class="card-body-icon">
+                                        <i class="fas fa-server fa-3x"></i>
+                                    </div>
+                                    <div class="mr-5">{{$subjects_count}} Subjects!</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-sm-6 mb-5">
+                            <div class="card text-white bg-success o-hidden">
+                                <div class="card-body">
+                                    <div class="card-body-icon">
+                                        <i class="fas fa-question-circle fa-3x"></i>
+                                    </div>
+                                    <div class="mr-5">{{$questions_count}} Questions!</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-sm-6 mb-5">
+                            <div class="card text-white bg-danger o-hidden">
+                                <div class="card-body">
+                                    <div class="card-body-icon">
+                                        <i class="fas fa-book fa-3x"></i>
+                                    </div>
+                                    <div class="mr-5">
+                                        {{$examinations_count}} Examinations!
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div><!--card-block-->
             </div><!--card-->
         </div><!--col-->
